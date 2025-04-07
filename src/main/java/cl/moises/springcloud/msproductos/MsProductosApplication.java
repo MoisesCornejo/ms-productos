5ae17082851e853e3a58a9b676559c8ff5cf09ad
MoisesCornejo <moises.cornejo@inacapmail.cl>
2025-04-06 23:10:18 -0400

    Inicializa el proyecto ms-productos con la configuración de Maven, Base de datos y Spring Boot.

---
+++ src/main/java/cl/moises/springcloud/msproductos/MsProductosApplication.java
@@ -0,0 +1,13 @@
+package cl.moises.springcloud.msproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProductosApplication.class, args);
+    }
+
+}
